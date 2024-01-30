0e9961b4baf7da6cc8eb9c33349f1f08cb2f5f69
prathi-112 <sowmyagoud0304@gmail.com>
2024-01-30 17:45:31 +0530

    style and script

---
+++ script.js
@@ -0,0 +1,39 @@
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+selectTag = document.querySelectorAll("select"),
+icons = document.querySelectorAll(".row i");
+translateBtn = document.querySelector("button"),
+
+selectTag.forEach((tag, id) => {
+    for (const country_code in countries) {
+        let selected;
+        if( id == 0 && country_code == "en-GB")  {
+        selected = " selected" ;
+        } else if(id == 1 && country_code == "te-IN"){
+           selected = "selected";
+        }
+        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+});
+ 
+
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value.trim(),
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+    if(!text) return;
+    toText.setAttribute("placeholder", "Translating...");
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        toText.value = data.responseData.translatedText;
+        data.matches.forEach(data => {
+            if(data.id === 0) {
+                toText.value = data.translation;
+            }
+        });
+     toText.setAttribute("placeholder", "Translation");   
+    });
+});
+
